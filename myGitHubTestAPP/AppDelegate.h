e08b10ce968d773a2293207913c7ef22853961bf
MichaelYinchuanZhou <yzhou088@gmail.com>
2016-04-22 12:31:46 -0500

    Initial Commit

---
+++ myGitHubTestAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myGitHubTestAPP
+//
+//  Created by Yinchuan Zhou on 4/22/16.
+//  Copyright © 2016 Yinchuan Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myGitHubTestAPP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  myGitHubTestAPP
+//
+//  Created by Yinchuan Zhou on 4/22/16.
+//  Copyright © 2016 Yinchuan Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
